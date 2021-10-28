6a30a2f175ee2a7d3603f5c6f6277afc7ee37351
ubuntu <dinesh99981@gmail.com>
2021-10-28 12:55:58 +0545

    mqtt publisher made to publish mission in SI unit

--- js/MissionCmd/MissionCmd.js
+++ js/MissionCmd/MissionCmd.js
@@ -49,51 +49,54 @@ function updatetAllMapObjects(currentIndex) {
 
 function missionCmdTopMenuServeBtnClicked()
 {
-    if(missionCmdTopMenu.robotComboBoxCurrentIndex>-1) {
-
-        flickableMap.updateWayPnts(missionCmdTopMenu.missionListModel.get(missionCmdTopMenu.missionComboBoxCurrentIndex).mission_id)
-        if(find(all_way_pnts, function(item) { return item.mission_id === missionCmdTopMenu.missionListModel.get(missionCmdTopMenu.missionComboBoxCurrentIndex).mission_id })) {
+    var robot_combobox_ind = missionCmdTopMenu.robotComboBoxCurrentIndex;
+    var mission_combobox_ind = missionCmdTopMenu.missionComboBoxCurrentIndex;
+    if(robot_combobox_ind > -1) {
+        flickableMap.updateWayPnts(missionCmdTopMenu.missionListModel.get(mission_combobox_ind ).mission_id)
+        if(find(all_way_pnts, function(item) { return item.mission_id === missionCmdTopMenu.missionListModel.get(mission_combobox_ind ).mission_id })) {
             way_pnts.append({
-                                 'mission_id': missionCmdTopMenu.missionListModel.get(missionCmdTopMenu.missionComboBoxCurrentIndex).mission_id,
+                                 'mission_id': missionCmdTopMenu.missionListModel.get(mission_combobox_ind ).mission_id,
                                  'mission_name': missionCmdTopMenu.missionComboBoxCurrentText
                            })
         }
 
-        if(!find(allActiveMission, function(item) { return item.active_mission === missionCmdTopMenu.missionListModel.get(missionCmdTopMenu.missionComboBoxCurrentIndex).mission_id+
-                                                    '_'+missionCmdTopMenu.robotListModel.get(missionCmdTopMenu.robotComboBoxCurrentIndex).robot_id }))
+        var mission_present = find(allActiveMission, function(item) { return item.active_mission === missionCmdTopMenu.missionListModel.get(mission_combobox_ind ).mission_id +
+                                                        '_' + missionCmdTopMenu.robotListModel.get(robot_combobox_ind).robot_id });
+        if(!mission_present)
         {
             allActiveMission.append({
-                                        'active_mission':   missionCmdTopMenu.missionListModel.get(missionCmdTopMenu.robotComboBoxCurrentIndex).mission_id+'_'+missionCmdTopMenu.robotListModel.get(missionCmdTopMenu.robotComboBoxCurrentIndex).robot_id,
-                                        'mission_id':       missionCmdTopMenu.missionListModel.get(missionCmdTopMenu.missionComboBoxCurrentIndex).mission_id,
-                                        'robot_id':         missionCmdTopMenu.robotListModel.get(missionCmdTopMenu.robotComboBoxCurrentIndex).robot_id,
-                                        'name':             missionCmdTopMenu.robotListModel.get(missionCmdTopMenu.robotComboBoxCurrentIndex).name})
+                                        'active_mission':   missionCmdTopMenu.missionListModel.get(robot_combobox_ind).mission_id+'_'+missionCmdTopMenu.robotListModel.get(robot_combobox_ind).robot_id,
+                                        'mission_id':       missionCmdTopMenu.missionListModel.get(mission_combobox_ind ).mission_id,
+                                        'robot_id':         missionCmdTopMenu.robotListModel.get(robot_combobox_ind).robot_id,
+                                        'name':             missionCmdTopMenu.robotListModel.get(robot_combobox_ind).name})
             activeRobotListModel.append({
-                                            'robot_id':     missionCmdTopMenu.robotListModel.get(missionCmdTopMenu.robotComboBoxCurrentIndex).robot_id,
-                                            'mission_id':   missionCmdTopMenu.missionListModel.get(missionCmdTopMenu.missionComboBoxCurrentIndex).mission_id,
-                                            'name':         missionCmdTopMenu.robotListModel.get(missionCmdTopMenu.robotComboBoxCurrentIndex).name })
+                                            'robot_id':     missionCmdTopMenu.robotListModel.get(robot_combobox_ind).robot_id,
+                                            'mission_id':   missionCmdTopMenu.missionListModel.get(mission_combobox_ind ).mission_id,
+                                            'name':         missionCmdTopMenu.robotListModel.get(robot_combobox_ind).name })
         }
-//        var data = ''
-//        all_way_pnts.clear()
-//        var wayPntsArray = JS.dbReadMissionWayPnts(missionCmdTopMenu.missionListModel.get(missionCmdTopMenu.missionComboBoxCurrentIndex).mission_id)
-//        for (var i = 0; i < wayPntsArray.rows.length; i++) {
-//            data += wayPntsArray.rows.item(i).x
-//            data += '_'
-//            data += wayPntsArray.rows.item(i).y
-//            data += '_'
-//            all_way_pnts.append({
-//                                       'name':  wayPntsArray.rows.item(i).name,
-//                                       'x':     wayPntsArray.rows.item(i).x,
-//                                       'y':     wayPntsArray.rows.item(i).y})
-//        }
-//        client.publish(missionCmdTopMenu.robotComboBoxCurrentText, data)
-//        missionCmdTopMenu.robotListModel.remove(missionCmdTopMenu.robotListModel.get(missionCmdTopMenu.robotComboBoxCurrentIndex))
-//        if(missionCmdTopMenu.robotListModel.count===0)
-//            missionCmdTopMenu.serveBtnEnabled = false
-//        else {
-//            var item = MyJS.find(all_way_pnts, function(item) { return item.name === 'Base' })
-//            if(item)
-//                flickableMap.createDynamicRobotItem(item.x, item.y, missionCmdTopMenu.robotListModel.get(missionCmdTopMenu.robotComboBoxCurrentIndex).name, false)
-//        }
+        var data = ''
+        all_way_pnts.clear()
+        var wayPntsArray = JS.dbReadMissionWayPnts(missionCmdTopMenu.missionListModel.get(mission_combobox_ind ).mission_id)
+        for (var i = 0; i < wayPntsArray.rows.length; i++) {
+            data += wayPntsArray.rows.item(i).x * 0.05 - 51.224998
+            data += '_'
+            data += (2048 - wayPntsArray.rows.item(i).y) * 0.05 - 51.224998
+            data += '_'
+            all_way_pnts.append({
+                                       'name':  wayPntsArray.rows.item(i).name,
+                                       'x':     wayPntsArray.rows.item(i).x,
+                                       'y':     wayPntsArray.rows.item(i).y
+                                })
+        }
+        client.publish(missionCmdTopMenu.robotComboBoxCurrentText, data)
+        missionCmdTopMenu.robotListModel.remove(missionCmdTopMenu.robotListModel.get(robot_combobox_ind))
+        if(missionCmdTopMenu.robotListModel.count===0)
+            missionCmdTopMenu.serveBtnEnabled = false
+        else {
+            var item = MyJS.find(all_way_pnts, function(item) { return item.name === 'Base' })
+            if(item)
+                flickableMap.createDynamicRobotItem(item.x, item.y, missionCmdTopMenu.robotListModel.get(robot_combobox_ind).name, false)
+        }
     }
 }
 
